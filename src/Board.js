185c1d1aa62d987a0d5e9fc0e538bffee875511e
aqlwah <akr.wee@gmail.com>
2020-03-13 01:50:24 +0900

    add: Board.js追加 #3

---
+++ src/Board.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Square from './Square';
+
+function Board() {
+  return (
+    <div>
+      Board Component Works!
+      <Square />
+    </div>
+  );
+}
+
+export default Board;
